8b39ae4477bcf5828ee7c734c81a596a73ac0940
adsuttman <adsuttman@gmail.com>
2023-03-29 09:50:45 -0400

    added waves of enemy spawns

--- game_manager.gd
+++ game_manager.gd
@@ -3,8 +3,8 @@ extends Node
 @onready var enemy_scene: PackedScene = load("res://enemy.tscn")
 #@onready var player_scene: PackedScene = load("res://player_character.tscn")
 var wave: int = 1
-var enemies_remaining: int = 1
-var spawn_count: int = 1
+var enemies_remaining: int = calculate_enemies(wave)
+var spawn_count: int = 0
 var spawn_points: Array = []
 var level: Node2D
 var spawn_timer: Timer
@@ -38,13 +38,28 @@ func spawn_enemy() -> void:
 	inst.global_position = random_spawn.global_position
 	level.add_child(inst)
 	inst.enemy_death.connect(handle_enemy_death)
+	spawn_count += 1
+	if spawn_count == calculate_enemies(wave):
+		spawn_timer.stop()
+
+func calculate_enemies(wave: int) -> int:
+	return wave * 2
 
 func increase_score() -> void:
 	score += 1
 	score_updated.emit(score)
 
+func handle_next_wave() ->void:
+	spawn_count = 0
+	wave += 1
+	enemies_remaining = calculate_enemies(wave)
+	spawn_timer.start()
+
 func handle_player_death() -> void:
 	spawn_timer.stop()
 
 func handle_enemy_death() -> void:
+	enemies_remaining -= 1
 	increase_score()
+	if enemies_remaining == 0:
+		handle_next_wave()
